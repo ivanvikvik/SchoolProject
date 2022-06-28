66299d2de856671a1134b252a0633f02560a575f
Victor <ivanvikvik@gmail.com>
2022-06-28 20:50:22 +0300

    Use @Before and @After methods with jUnit

--- src/by/itstep/vikvik/javalessons/lesson22/model/logic/SchoolManager.java
+++ src/by/itstep/vikvik/javalessons/lesson22/model/logic/SchoolManager.java
@@ -3,24 +3,55 @@ package by.itstep.vikvik.javalessons.lesson22.model.logic;
 public class SchoolManager {
     public static double calculateAvgMark(int[][] groups) {
         double sum = 0;
+        int count = 0;
 
         for (int i = 0; i < groups.length; i++) {
-            sum += calculateGroupAvgMark(groups[i]);
+            for (int j = 0; j < groups[i].length; j++) {
+                sum += groups[i][j];
+            }
+            count += groups[i].length;
         }
 
-        return sum / groups.length;
+        return sum / count;
     }
 
-    private static double calculateGroupAvgMark(int[] group) {
-        double sum = 0;
-        for (int i = 0; i < group.length; i++) {
-            sum += group[i];
+    public static String findBadStudentGroups(int[][] groups) {
+        String result = "";
+        for (int i = 0; i < groups.length; i++) {
+            for (int j = 0; j < groups[i].length; j++) {
+                if (groups[i][j] < 4) {
+                    result += (i + 1) + " ";
+                    break;
+                }
+            }
         }
-        return sum / group.length;
+        return result.trim();
     }
+
+    public static String findGroupsWithoutBadStudents(int[][] groups) {
+        String result = "";
+
+        for (int i = 0; i < groups.length; i++) {
+            boolean flag = true;
+
+            for (int j = 0; j < groups[i].length; j++) {
+                if (groups[i][j] < 4) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag) {
+                result += (i + 1) + " ";
+            }
+        }
+
+        return result.trim();
+    }
+
 }
 
-class Test{
+class Test {
     public static void main(String[] args) {
 
     }
--- test/by/itstep/vikvik/javalessons/lesson22/model/logic/SchoolManagerTest.java
+++ test/by/itstep/vikvik/javalessons/lesson22/model/logic/SchoolManagerTest.java
@@ -2,19 +2,76 @@ package by.itstep.vikvik.javalessons.lesson22.model.logic;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SchoolManagerTest {
+
+    private int[][] groups;
+
+    @Before
+    public void init() {
+        System.out.println("@Before");
+        groups = new int[][]{
+                {7, 7, 8, 2},
+                {9, 8, 9, 10},
+                {7, 7, 1},
+                {8, 8, 10, 10}
+        };
+    }
+
+    @After
+    public void destroy(){
+        System.out.println("@After");
+        groups = null;
+    }
+
+    @Before
+    public void init1() {
+        System.out.println("@Before");
+    }
+
+    @Before
+    public void init2() {
+        System.out.println("@Before");
+    }
+
     @Test
     public void testCalculateAvgMarkPositive() {
-        int[][] groups = {
-                {7, 8, 9},      // 8.0
-                {9, 8, 9, 10},  // 9.0
-                {7, 7}};        // 7.0
-        double expected = 8.0;
+        System.out.println("testCalculateAvgMarkPositive");
+        //int[][] groups = getGroups();
+
+        double expected = 7.4;
 
         double actual = SchoolManager.calculateAvgMark(groups);
 
-        assertEquals(expected, actual, 0.01);
+        assertEquals(expected, actual, 0.5);
+    }
+
+    @Test
+    public void testFindBadStudentGroupsPositive() {
+        System.out.println("testFindBadStudentGroupsPositive");
+
+        //int[][] groups = getGroups();
+
+        String expected = "1 3";
+
+        String actual = SchoolManager.findBadStudentGroups(groups);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testfindGroupsWithoutBadStudentsPositive() {
+        System.out.println("testfindGroupsWithoutBadStudentsPositive");
+
+        //int[][] groups = getGroups();
+
+        String expected = "2 4";
+
+        String actual = SchoolManager.findGroupsWithoutBadStudents(groups);
+
+        assertEquals(expected, actual);
     }
 }
